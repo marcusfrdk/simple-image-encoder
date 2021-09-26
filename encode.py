b81dd854fe73779d4c22fa50370a8ce0d0f68aba
Marcus Fredriksson <marcus@marcusfredriksson.com>
2021-09-26 08:20:53 +0200

    Working on new encoder, working

--- encode.py
+++ encode.py
@@ -1,33 +1,13 @@
 import argparse
 import os
+import time
 
 def get_arguments() -> dict:
     parser = argparse.ArgumentParser(description="Encode strings or files to an image as JSON.", usage="encode.py path")
-    parser.add_argument("path", help="path to the image to be encoded, must be jpg or png")
-    parser.add_argument("-s", "--string", dest="string", metavar="", help="store the string in an image")
-    parser.add_argument("-f", "--file", dest="file", metavar="", help="stringify a file and store it in an image")
+    parser.add_argument("image", help="path to the image to be encoded.")
+    parser.add_argument("data", help="data to be encoded, if the string is a path, the file will be encoded, else the provided string will be used.")
     parser.add_argument("-n", "--name", dest="name", metavar="", help="saves the encoded image with the name, will overwrite any existing file with the same name.")
     return parser.parse_args()
-
-
-def validate_image(path: str) -> bool:
-    valid_extensions = ("jpg", "jpeg", "png")
-
-    is_file = os.path.isfile(path)
-    extension_is_valid = path.split(".")[-1] in valid_extensions \
-        if "." in path else False
-
-    return is_file and extension_is_valid
-
-
-def parse_input(args: dict) -> str:
-    if args.string:
-        return str(args.string)
-    elif args.file:
-        with open(args.file) as f:
-            return str(f.read())
-    else:
-        return ""
 
 
 def extract_path(path: str) -> tuple:
@@ -73,18 +53,75 @@ def encode(args: dict, input: str) -> None:
     except:
         print("Failed to encode image, please try again.")
 
+
+# def get_encoded_data(image: str) -> str:
+#     encoded_data = ""
+
+#     with open(image, "rb") as f:
+#         content = f.read()
+#         offset = content.index(bytes.fromhex("FFD9"))
+
+#         f.seek(offset + 2)
+#         encoded_data = bytes.decode(f.read())
+
+#     return encoded_data
+
+
+def parse_data(data: str) -> str:
+    is_file = os.path.isfile(data)
+    result = data
+    if is_file:
+        with open(data) as f:
+            result = f.read()
+    return result
+
+
+def format_data(data: str) -> bytes:
+    is_file = os.path.isfile(data)
+    file_type = data.split(".")[-1] if "." in data and is_file else ""
+    encoded_at = time.time()
+
+    return bytes(str({
+        "file_type": file_type,
+        "encoded_at": encoded_at,
+        "data": parse_data(data)
+    }), "utf-8")
+
+
+def validate_image(path: str) -> bool:
+    valid_extensions = ("jpg", "jpeg", "png")
+
+    is_file = os.path.isfile(path)
+    extension_is_valid = path.split(".")[-1] in valid_extensions \
+        if "." in path else False
+
+    return is_file and extension_is_valid
+
+
+def encode_image(args: dict)  -> None:
+    data = format_data(args.data)
+
+    # Get image and remove encoding
+    image = ""
+    with open(args.image, "rb") as of:
+        content = of.read()
+        offset = content.index(bytes.fromhex("FFD9")) + 2
+        image = content[0:offset]
+        of.close()
+
+    with open("test.jpg", "wb") as nf:
+        nf.write(image + data)
+        nf.close()
+
+
 def main() -> None:
     args = get_arguments()
-    
-    if validate_image(args.path):
-        input = parse_input(args)
 
-        if input:
-            encode(args, input)
-        else:
-            print("Nothing to encode, please use the -s or -f flags.")
+    if validate_image(args.image):
+        encode_image(args)
     else:
-        print("Image is not valid, try another one.")
+        print("Image is not valid.")
+        
 
 
 if __name__ == "__main__":
